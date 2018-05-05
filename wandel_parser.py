a9e16b8fb999d4905decf58d94611c2bbb34a699
sebastian-dasse <sebastian.dasse@mailbox.org>
2018-05-05 15:52:32 +0200

    Refactor wandel_parser a bit

--- wandel_parser.py
+++ wandel_parser.py
@@ -56,21 +56,32 @@ def menu_title(menu_path):
 
 
 def extract_meals(text):
+  def is_valid(chunk):
+    return re.match(category_title_pattern, chunk)
+
+  def normalised_title(title_line):
+    for category in categories:
+      if category in title_line:
+        return title_for_category[category]
+  
+  def meal_from(chunk):
+    title_line, meal_chunk = chunk.split("\n", 1)
+    return normalised_title(title_line), meal_chunk
+  
+  def description_from(meal_chunk):
+    meal_chunks = re.split(meal_delimiter_pattern, meal_chunk)
+    missing = [""] * (len(categories) - len(meal_chunks))
+    return ([meal.replace("\n", "") for meal in meal_chunks] + missing)[:num_week_days]
+  
   menu = {}
   error_count = 0
   for chunk in re.split(category_delimiter_pattern, text):
-    if re.match(category_title_pattern, chunk):
+    if is_valid(chunk):
       try:
-        title_line, meal_chunk = chunk.split("\n", 1)
-        title = _normalised_title(title_line)
-        
+        title, meal_chunk = meal_from(chunk)
         if title in menu:
           continue
-        
-        meal_chunks = re.split(meal_delimiter_pattern, meal_chunk)
-        missing = [""] * (len(categories) - len(meal_chunks))
-        menu[title] = ([meal.replace("\n", "") for meal in meal_chunks] + missing)[:num_week_days]
-
+        menu[title] = description_from(meal_chunk)
       except ValueError:
         error_count += 1
 
@@ -78,9 +89,3 @@ def extract_meals(text):
     print "There were " + str(error_count) + " while parsing the menu"
 
   return menu
-
-
-def _normalised_title(title_line):
-  for category in categories:
-    if category in title_line:
-      return title_for_category[category]
